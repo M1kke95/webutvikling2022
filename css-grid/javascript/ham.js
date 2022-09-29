ae302259191a9536c4de01a2650a00beac276a1a
M1kke95 <m1kke95@hotmail.com>
2022-09-29 12:42:17 +0200

    :)

---
+++ css-grid/javascript/ham.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("main-menu");
+
+const hamMenu = () =>{
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+
+    }
+    else{
+        menu.style.display = "block";
+    }
+}
+
+
+
+
